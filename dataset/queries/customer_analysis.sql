f003551cc91ed7c7e41bb5b0982f297f9f660c44
Kojagiri20 <kojagirichavan20@gmail.com>
2026-03-06 15:20:40 +0530

    Added customer segmentation and high value customer analysis

--- dataset/queries/customer_analysis.sql
+++ dataset/queries/customer_analysis.sql
@@ -25,7 +25,40 @@ FROM sales_data_sample
 GROUP BY CUSTOMERNAME
 ORDER BY total_orders DESC;
 
+--Customer Segmentation by revenue
+SELECT 
+   CUSTOMERNAME,
+   ROUND(SUM(sales),2) AS total_revenue,
+CASE
+   WHEN SUM(SALES) > 100000 'High Value'
+   WHEN SUM(SALES) BETWEEN 50000 AND 100000 THEN 'Medium Value'
+        ELSE 'Low Value'
+END AS customer_segment
+FROM sales_data_sample
+GROUP BY CUSTOMERNAME
+ORDER BY total_revenue
 
+--Count High value Customers
+SELECT 
+   COUNT(*) AS High_value_customers
+FROM (
+      SELECT CUSTOMERNAME
+      FROM sales_data_sample
+      GROUP BY CUSTOMERNAME
+      HAVING SUM(sales) > 100000
+) AS sub;
+
+--Percentage of High value customers
+SELECT 
+   ROUND(COUNT(*) * 100.0 /
+   (SELECT COUNT(DISTINCT CUSTOMERNAME) AS sales_data_sample),2)
+   AS high_value_customer_percentage
+FROM(
+   SELECT CUSTOMERNAME
+   FROM sales_data_sample
+   GROUP BY CUSTOMERNAME
+   HAVING SUM(sales) > 100000
+) AS sub;
 
 
 

b95531a4512d58e4e413f89539f4067770f50e15
Kojagiri20 <kojagirichavan20@gmail.com>
2026-03-06 15:30:03 +0530

    Added advanced SQL analysis using CTE and window functions
    Added a CTE to calculate revenue share of top 20% customers using a window function.

--- dataset/queries/advanced_analysis.sql
+++ dataset/queries/advanced_analysis.sql
@@ -25,6 +25,26 @@ FROM sales_data_sample
 GROUP BY YEAR(ID)
 ORDER BY yearly_revenue DESC;
 
+-- CTE + WINDOW function
+--Revenue Share of Top 20% share
 
+WITH customer_revenue as(
+   SELECT CUSTOMERNAME,
+   SUM(sales) AS total_revenue
+   FROM sales_data_sample
+   GROUP BY CUSTOMERNAME
+),
+ranked_customer AS(
+   SELECT *,
+       NTILE(5) OVER(ORDER BY total_revenue) AS revenue_group
+   FROM customer_revenue
+),
+SELECT 
+   revenue_group
+   ROUND(SUM(total_revenue),2) AS group_revenue
+FROM ranked_customer
+GROUP BY revenue_group 
+ORDER BY revenue_group
+   
 
 
